7298308054df8cee8c2c84e87446913e05062306
Mustafa Iraqi <118061209+Telegram-R2RR7@users.noreply.github.com>
2024-06-01 04:13:48 +0300

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -68,7 +68,7 @@ class Var:
     LOG_FILE = config("LOG_FILE", default="Stream.log")
 
     TWITTER_USERNAME = config("TWITTER_USERNAME", default="R2RR70")
-    TWITTER_PASSWORD = config("TWITTER_PASSWORD", default="Mohammed2009@@#")
+    TWITTER_PASSWORD = config("TWITTER_PASSWORD", default="Mohammed2009@@@")
     ACCOUNTS_FILE = config("ACCOUNTS_FILE", default="")
 
     DELAY_MINUTES = config("DELAY_MINUTES", default=1, cast=int)
