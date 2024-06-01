b2338943ea8cc141650b8e36a714b9d6371ffb2b
Mustafa Iraqi <118061209+Telegram-R2RR7@users.noreply.github.com>
2024-06-01 03:24:12 +0300

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -18,7 +18,7 @@ class Var:
 
     # Username of Twitter User, whose Tweets should be tracked
     # and posted to chat filled in TO_CHAT.
-    TRACK_USERS = config("TRACK_USERS", default="bt3 R2RR70 Copa_Lal TrendEPL theMadridZone forcabarca_ar")
+    TRACK_USERS = config("TRACK_USERS", default="fabrizioRomano IraqXtra_ar MadridXtra M30Xtra RM4Arab")
     # TRACK_WORDS: To filter Tweets by word
     # Should be seperated by "|"
     TRACK_WORDS = config("TRACK_WORDS", default="")
