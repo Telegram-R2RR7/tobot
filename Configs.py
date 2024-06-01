976c7271c9e8adac4a0407fa0353bd8103ec4415
Mustafa Iraqi <118061209+Telegram-R2RR7@users.noreply.github.com>
2024-06-01 09:35:56 +0300

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -7,18 +7,19 @@ from decouple import config
 
 class Var:
     # Telegram's API ID
-    API_ID = config("API_ID", default=14990654)
+    API_ID = config("API_ID", default=6)
     # Telegram's API HASH
-    API_HASH = config("API_HASH", default="c6d66eb4d2a5e33e54834e0cbc305c2d")
+    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
     # Telegram Bot's token
-    BOT_TOKEN = config("BOT_TOKEN", default="7132646032:AAGUDvx5djLXewJ2AsDgiAW1-JmMD6necw4")
+    BOT_TOKEN = config("BOT_TOKEN", None)
 
     # Telegram Chat id(s), where to send Tweets
-    TO_CHAT: str = config("TO_CHAT", default="-1002176802148")
+    TO_CHAT: str = config("TO_CHAT", None)
 
     # Username of Twitter User, whose Tweets should be tracked
     # and posted to chat filled in TO_CHAT.
-    TRACK_USERS = config("TRACK_USERS", default="bt3 R2RR70 Copa_Lal TrendEPL theMadridZone forcabarca_ar fabrizioRomano IraqXtra_ar MadridXtra M30Xtra RM4Arab")
+    TRACK_USERS = config("TRACK_USERS", default="")
+
     # TRACK_WORDS: To filter Tweets by word
     # Should be seperated by "|"
     TRACK_WORDS = config("TRACK_WORDS", default="")
@@ -26,7 +27,7 @@ class Var:
     # Custom Text format to be used, while sending Tweets.
     CUSTOM_TEXT = config("CUSTOM_TEXT", default="")
     # Text to Display on Button, Attached to Message Posted on Telegram.
-    BUTTON_TITLE = config("BUTTON_TITLE", "مشاهدة التغريدة")
+    BUTTON_TITLE = config("BUTTON_TITLE", "View on Twitter🔗")
     # Set DISABLE_BUTTON to True, to disable that Button.
     CUSTOM_BUTTON = config("CUSTOM_BUTTON", default=None)
     DISABLE_BUTTON = config("DISABLE_BUTTON", default=False, cast=bool)
@@ -67,8 +68,8 @@ class Var:
     MEDIA_DL_PATH = config("MEDIA_DL_PATH", default="media")
     LOG_FILE = config("LOG_FILE", default="Stream.log")
 
-    TWITTER_USERNAME = config("TWITTER_USERNAME", default="R2RR70")
-    TWITTER_PASSWORD = config("TWITTER_PASSWORD", default="Mohammed2009@@@")
+    TWITTER_USERNAME = config("TWITTER_USERNAME", default="")
+    TWITTER_PASSWORD = config("TWITTER_PASSWORD", default="")
     ACCOUNTS_FILE = config("ACCOUNTS_FILE", default="")
 
     DELAY_MINUTES = config("DELAY_MINUTES", default=1, cast=int)
